59d37c15a1cf0ed42a33509b81080a7763501b8e
FCI <fci@FCI1988s-MacBook-Pro.local>
2024-02-07 12:46:43 +0530

    visa screen done

---
+++ TravgateApp/APP/scens/Visa/Views/CVCells/AutopayTabCVCell/AutopayTabCVCell.swift
@@ -0,0 +1,17 @@
+//
+//  AutopayTabCVCell.swift
+//  TravgateApp
+//
+//  Created by FCI on 07/02/24.
+//
+
+import UIKit
+
+class AutopayTabCVCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ TravgateApp/APP/scens/Visa/Views/TVCells/AutoPaymentTVCell/AutoPaymentTVCell.swift
@@ -0,0 +1,23 @@
+//
+//  AutoPaymentTVCell.swift
+//  TravgateApp
+//
+//  Created by FCI on 07/02/24.
+//
+
+import UIKit
+
+class AutoPaymentTVCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
